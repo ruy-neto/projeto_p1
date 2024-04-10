f55bfb2dab641e7745f2b55583ef516ff80b2a05
ruy-neto <2203113@aluno.univesp.br>
2024-04-09 23:00:49 -0300

    feat: Criado a tela de login.

--- projeto-tag/app/login/page.js
+++ projeto-tag/app/login/page.js
@@ -1,12 +1,34 @@
+"use client"
 import React from 'react';
+import { Container, Form, Button } from 'react-bootstrap';
 
-const LoginPage = () => {
+const LoginForm = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Aqui você pode adicionar a lógica para lidar com os dados do formulário (autenticação, validação, etc.)
+    console.log('Formulário enviado!');
+  };
+
+
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>This is the about page content.</p>
-    </div>
+    <Container className="vh-100 d-flex justify-content-center align-items-center">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Usuário</Form.Label>
+          <Form.Control required />
+        </Form.Group>
+        <p></p>
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Senha</Form.Label>
+          <Form.Control type="password" required />
+        </Form.Group>
+        <p></p>
+        <Button variant="primary" type="submit">
+          Entrar
+        </Button>
+      </Form>
+    </Container>
   );
 };
 
-export default LoginPage;
+export default LoginForm;
