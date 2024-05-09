a69aaae891583d0d39f9773a889339f537391bd9
ruy-neto <2203113@aluno.univesp.br>
2024-05-08 22:24:12 -0300

    heroku configuration

--- projecto/src/main.ts
+++ projecto/src/main.ts
@@ -16,6 +16,6 @@ async function bootstrap() {
     defaultLayout: 'layout',
     layoutsDir: join(__dirname, '..', 'views', 'layouts')
   }));
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
